2004f996ed09e6c9e31ae785bc0016095fb412c9
Jorge <newdarkjg@gmail.com>
2023-11-07 15:39:12 +0100

    Commit final

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
  // Aquí tu código
+const boton=document.getElementById("agregar");
+const lista= document.getElementById("lista");
+console.log(boton)
+
+boton.addEventListener("click",function(){
+    let dato=prompt("Introduce el dato");
+    lista.innerHTML+="<li>"+dato+"</li>";
+})
